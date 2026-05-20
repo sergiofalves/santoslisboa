81fc58d567426df106ac35c2d00c0331e9238bb1
Sergio <sergioalves.194@gmail.com>
2026-05-20 13:55:15 +0100

    analytics info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 
 export const metadata: Metadata = {
   title: "Festas de Lisboa 2026",
@@ -30,6 +32,7 @@ export default function RootLayout({
       </head>
       <body>
         <Providers>{children}</Providers>
+        <GoogleAnalytics gaId="G-6JSFQPH1QW" />
       </body>
     </html>
   );
